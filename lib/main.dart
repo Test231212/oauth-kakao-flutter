b33015a08140edb8e266f7ed64efb87e87d14bb0
test231212 <j910219@gmail.com>
2024-06-03 15:30:35 +0900

    완료!

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'package:oauthapp/_core/move.dart';
 import 'ui/login_page/login_page.dart';
 
 GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 void main() {
+  // 웹 환경에서 카카오 로그인을 정상적으로 완료하려면 runApp() 호출 전 아래 메서드 호출 필요
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // runApp() 호출 전 Flutter SDK 초기화
+  KakaoSdk.init(nativeAppKey: 'a9cd04d4bf06b20230cf34e484175aa1',);
   runApp(ProviderScope(child: MyApp()));
 }
 
